0aacc6fd9ca2fe70e3ed66593d1bb1658911a92d
Sangmitra06 <sm20pd@brocku.ca>
2021-12-06 14:15:30 +0530

    final website

--- Final website /js1.js
+++ Final website /js1.js
@@ -13,20 +13,24 @@ function checkButton(){
     alert("Select a region");
   }
 }
-function backHome(){
-  window.location="Home_page.html"
-}
+
 function increase(){
   i++;
   document.getElementById('numIncrease').innerHTML=i;
 }
 setInterval('increase()',1000);
-function confirm(){
+
+function sendMail(){
   if(document.getElementById('email').value==""){
     alert("Please enter an email");
   }
   else{
-    document.getElementById('message').value="Want to send another query?";
-    alert("Sent!");
+  // Gabriel. (2015, July). How to send HTML form data to email. Retrieved on 3rd December, 2021. Website. https://stackoverflow.com/questions/31435012/how-to-send-html-form-data-by-email-to-an-email-id-by-using-javascript-without
+    var link = "mailto:me@example.com"
+         + "?cc=myCCaddress@example.com"
+         + "&subject=" + escape("Queries/Comments")
+         + "&body=" + escape(document.getElementById('email').value)  + escape("\n") + escape(document.getElementById('message').value);
+    window.location.href=link;
+    document.getElementById("message").value="Send another query";
   }
 }
